f8f0a10a166b391018c843a14febe084921dfc3e
benny <bennyljq@gmail.com>
2023-07-31 22:39:52 +0800

    completed mock homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: HomepageComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' }, // redirect to `homepage`
+  // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
