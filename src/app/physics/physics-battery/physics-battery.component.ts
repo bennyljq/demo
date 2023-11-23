072f63a16e2e1f45d34716eee9fd05dea58210ea
benny <bennyljq@gmail.com>
2023-11-24 02:51:12 +0800

    added battery charging animation

--- src/app/physics/physics-battery/physics-battery.component.ts
+++ src/app/physics/physics-battery/physics-battery.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ThemeService } from '../theme.service';
+import { lastValueFrom, timer } from 'rxjs';
 
 @Component({
   selector: 'app-physics-battery',
@@ -19,6 +20,10 @@ export class PhysicsBatteryComponent {
   hideBack = true
   hideNext = false
   preventNext = false
+
+  ngOnInit() {
+    this.init_battery_percent()
+  }
 
   ngOnChanges() {
     if (this.selectedPageIndex == -1) {
@@ -62,18 +67,15 @@ export class PhysicsBatteryComponent {
         this.hideBack = false
         break
     }
-    this.init_question()
-  }
-  onDotClick(index: number) {
-    if (this.preventNext && index > this.selectedPageIndex) { return }
-    this.selectedPageIndex = index; 
-    this.setButtons()
   }
 
   answer_options = ['66000 J', '67000 J', '68000 J', '69000 J']
   correct_answer = 2
   picked_answer: number | undefined;
   question_answered = false
+  charging_array = Array(5).fill(0)
+  battery_percent = 12
+  charge_timer = 0
 
   energy_delivered = "$25J \\times 40 \\times 60 = 60000J$";
   total_capacity = "$60000J \\div 0.88 \\approx 68182J$";
@@ -83,20 +85,20 @@ export class PhysicsBatteryComponent {
   mah_power_2 = "$5A \\times 3.8V = 19W$"
   mah_capacity = "$19W \\times 3600s = 68400J$"
 
-  init_question() {
-    if (this.question_answered) { return }
-    if (this.selectedPageIndex == 1) {
-      this.hideNext = true
-      this.preventNext = true
-    } else {
-      this.preventNext = false
+  async init_battery_percent() {
+    while (true) {
+      if (this.battery_percent == 100 || this.battery_percent == 12) {
+        await lastValueFrom(timer(2000))
+      } else {
+        await lastValueFrom(timer(40))
+      }
+      this.battery_percent = (this.battery_percent - 11) % 89 + 12
+      this.charge_timer = Math.floor((this.battery_percent - 12) / 88 * 40)
     }
   }
   on_answer(answer_index: number) {
     this.picked_answer = answer_index
     this.question_answered = true
-    this.hideNext = false
-    this.preventNext = false
   }
   
 }
