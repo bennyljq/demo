0a34d890d70f6a444252a8fe6bc462ec6bcad3f9
Benny Lim <benny.lim@bennyli-ltm7cmt.internal.salesforce.com>
2026-03-09 17:31:45 -0700

    added multi touch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,6 +24,7 @@ import { Gravity2025Component } from './gravity2025/gravity2025.component';
 import { TypingHomepageComponent } from './typing-game/typing-homepage/typing-homepage.component';
 import { OrdinalComponent } from './ordinal/ordinal.component';
 import { ShapeBisectorComponent } from './shape-bisector/shape-bisector.component';
+import { Gravity2026Component } from './gravity-2026/gravity-2026.component';
 
 const routes: Routes = [
   { path: '', component: HomepageV2Component },
@@ -54,6 +55,7 @@ const routes: Routes = [
       { path: '**', redirectTo: '' }
     ]
   },
+  { path: 'gravity-2026', component: Gravity2026Component },
   { path: '**', redirectTo: '', pathMatch: 'full'},  // Wildcard route for a 404 page, have not gotten to this
 ];
 
